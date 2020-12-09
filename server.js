4b265f6e93d9e924759c13338eac1de71a6b2e44
Ilya <0ilya@bk.ru>
2020-12-09 11:25:40 +0300

    change get

--- server.js
+++ server.js
@@ -7,5 +7,8 @@ const port = process.env.PORT || 3000;
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/prod/index.html'));
 });
-
+app.get('/:name', function(req, res) {
+    let name = req.params.name;
+    res.sendFile(path.join(__dirname + '/prod/' + name));
+});
 app.listen(port, () => console.log(`listening on port ${port}!`));
