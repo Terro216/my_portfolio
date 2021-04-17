f9d680fb752c2b7b8b06268e8a5eb72d2518a564
Terro216 <0ilya@bk.ru>
2021-04-17 13:25:49 +0300

    add links to projects

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ import { render } from "react-dom";
 function App() {
   const [state, setState] = useState("CLICK ME");
 
-  return <button onClick={() => setState("CLICKED")}>{state}</button>;
-}
+  return (
+  <div>
+  <h1>Projects:</h1>
+  <ul>
+    <li><a target='_blank' href="https://terro216.github.io/fcc-portfolio/">fcc portfolio (timer,drumpad,etc)</a></li>
+    <li><a target='_blank' href="https://terro216.github.io/cinema-react/">react-cinema</a></li>
+  </ul>
+  <button onClick={() => setState("CLICKED")}>{state}</button>
+  </div>
+  );
+} 
 
 render(<App />, document.getElementById("root"));
