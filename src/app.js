e3d50b9275fec1e1e08cefb4a3cf948733f3a4a0
Terro216 <0ilya@bk.ru>
2022-02-01 03:06:09 +0300

    update text + animation + mob version

--- src/app.js
+++ src/app.js
@@ -1,44 +1,60 @@
-import React, {useEffect, useState} from "react";
-import ReactDOM from "react-dom";
-import './app.scss';
-import './files/particles.min.js';
-import { useSpring, animated } from 'react-spring'
-import Header from "./components/header.js";
-import Modal from './components/modal.js';
-import Instruments from "./components/instruments.js";
-import Portfolio from "./components/portfolio.js";
-import Contact from "./components/contact.js";
+import React, { useEffect, useState } from "react"
+import ReactDOM from "react-dom"
+import "./app.scss"
+import "./files/particles.min.js"
+import { useSpring, animated } from "react-spring"
+import Header from "./components/header.js"
+import Modal from "./components/modal.js"
+import Instruments from "./components/instruments.js"
+import Portfolio from "./components/portfolio.js"
+import Contact from "./components/contact.js"
 
-function App() { //поменять текст + ограничить колво частиц
+function App() {
+	const [modalState, changeState] = useState({
+		state: 0,
+		modalImg: undefined,
+		modalTitle: "",
+		modalDesc: "",
+		modalSite: "",
+		modalSrc: "",
+	})
 
-    const [modalState, changeState] = useState({
-        state:0,
-        modalImg: undefined,
-        modalTitle: '',
-        modalDesc:'',
-        modalSite:'',
-        modalSrc:''
-    });
+	const bodyFade = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } })
 
-    const bodyFade = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } });
+	function toggleModal(img, title, desc, site, src) {
+		modalState.state === 0
+			? changeState({
+					state: 1,
+					modalTitle: title,
+					modalImg: img,
+					modalDesc: desc,
+					modalSite: site,
+					modalSrc: src,
+			  })
+			: changeState({ state: 0 })
+	}
 
-    function toggleModal(img,title,desc,site,src) {
-        (modalState.state===0)?changeState({state:1,modalTitle:title,modalImg:img,modalDesc:desc,modalSite:site,modalSrc:src}):changeState({state:0});
-    }
+	return (
+		<animated.div style={bodyFade} className="app-wrapper">
+			<Modal
+				exit={toggleModal}
+				state={modalState.state}
+				img={modalState.modalImg}
+				title={modalState.modalTitle}
+				desc={modalState.modalDesc}
+				site={modalState.modalSite}
+				src={modalState.modalSrc}
+			/>
 
-    return(
-        <animated.div style={bodyFade} className='app-wrapper'>
-        <Modal exit={toggleModal} state={modalState.state} img={modalState.modalImg} title={modalState.modalTitle} desc={modalState.modalDesc} site={modalState.modalSite} src={modalState.modalSrc} />
+			<Header />
 
-        <Header />  
-        
-        <Instruments/>
+			<Instruments />
 
-        <Portfolio toggler={toggleModal}/>
-        
-        <Contact />
-        </animated.div>
-    );
+			<Portfolio toggler={toggleModal} />
+
+			<Contact />
+		</animated.div>
+	)
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"))
--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,35 @@
-import React from "react";
-import dreamer from '../files/dreamer.png';
-import './header.scss'
+import React from "react"
+import dreamer from "../files/dreamer.png"
+import "./header.scss"
 
 function Header() {
-return(
-    <header className='welcome'>
-        <div id="particles-js"></div>
-        <div className='welcome-img'>
-                <figure>
-                    <h1 className='mobHello'>Привет!</h1><br></br>
-                    <img className='me' src={dreamer}>
-                    </img>
-                    <p></p>
-                    <figcaption>Это я</figcaption>
-                </figure>
-                </div>
-                <div className='welcome-text'>
-                    <h1 className='pcHello'>Привет!</h1><br></br>
-                    <h3>Меня зовут Илья, мне 18 лет и я занимаюсь web-разработкой.<br/> Я считаю, что программирование - это искусство и всегда ответственно подхожу к порученным мне задачам.</h3>
-                    
-                </div>
+	return (
+		<header className="welcome">
+			<div id="particles-js"></div>
+			<div className="welcome-img">
+				<figure>
+					<h1 className="mobHello">Привет!</h1>
+					<br></br>
+					<img className="me" src={dreamer}></img>
+					<p></p>
+					<figcaption>Это я</figcaption>
+				</figure>
+			</div>
+			<div className="welcome-text">
+				<h1 className="pcHello">Привет!</h1>
+				<br></br>
+				<h3>
+					Меня зовут Илья, мне 19 лет и я занимаюсь web-разработкой.
+					<br /> Я считаю, что программирование - это искусство, всегда ответственно подхожу к порученным мне
+					задачам и люблю чёрный чай больше зелёного.
+				</h3>
+			</div>
 
-                
-                <div className='arrow'>
-                    <i className="fa fa-arrow-down" aria-hidden="true" ></i>
-                </div>
-            </header>
-);
+			<div className="arrow">
+				<i className="fa fa-arrow-down" aria-hidden="true"></i>
+			</div>
+		</header>
+	)
 }
 
-export default Header;
+export default Header
